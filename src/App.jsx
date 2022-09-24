729457401cf5082083f824f082df570e09b46467
Eliseo Espindola <eliseoespindola1@gmail.com>
2022-09-24 16:24:43 -0300

    agregue el punto 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import HelloWorld from "./components/HelloWorld";
 function App() {
+  let saludo = 'My Friend'
   return (
     <>
     <HelloWorld></HelloWorld>
+    <HelloWorld saludar={saludo}></HelloWorld>
     </>
   );
 }
--- src/components/HelloWorld.jsx
+++ src/components/HelloWorld.jsx
@@ -1,8 +1,8 @@
 
-const HelloWorld = () => {
+const HelloWorld = (props) => {
     return (
         <div>
-            <h1>Hello World</h1>
+            <h1 props>Hello World {props.saludar}</h1>
         </div>
     );
 };
