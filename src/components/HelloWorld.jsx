632ffeafe0d9c23aa0f2f3b60eb1ff78a3bacea3
Eliseo Espindola <eliseoespindola1@gmail.com>
2022-09-24 16:40:55 -0300

    realice el ejercicio n°3

--- src/components/HelloWorld.jsx
+++ src/components/HelloWorld.jsx
@@ -1,8 +1,13 @@
+import { useState } from "react";
 
 const HelloWorld = (props) => {
+    let [claveMje, setclaveMje] = useState('Hello World My Friend')
     return (
         <div>
             <h1 props>Hello World {props.saludar}</h1>
+            <h1>{claveMje}</h1>
+            <button onClick={()=>setclaveMje('(from changed state)')}>Cambiar Mensaje</button>
+            <button onClick={()=>setclaveMje('Hello World My Friend')}>Reestablecer Mensaje</button>
         </div>
     );
 };
